a380c005942f4590eb5841e8160668427a9cdabc
Matt Im <matthew.seiim@gmail.com>
2020-05-01 16:40:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def summon_captain_planet(planeteer_calls)
     collection << yield(planeteer_calls[i])
     i+=1
   end
-  collection.capitalize
+  collection
 end
 
 def long_planeteer_calls# code an argument here
