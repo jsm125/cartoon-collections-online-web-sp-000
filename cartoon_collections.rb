5a6f83356e6f7f2b836c853c512fa9ec4f4abb11
Matt Im <matthew.seiim@gmail.com>
2020-05-01 16:52:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ end
 
 def summon_captain_planet(planeteer_calls)
   # Your code here
-  planeteer_calls.map { |calls|
+  planeteer_calls.map { |planeteer_calls|
 end
 
 def long_planeteer_calls# code an argument here
