1a700b26dedfd6b74dc2b837df107c4d75548fcf
Matt Im <matthew.seiim@gmail.com>
2020-05-01 17:20:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,8 +7,9 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map { |planeteer_calls| planeteer_calls.capitalize + '!'}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(short words)# code an argument here
   # Your code here
+  
 end
 
 def find_the_cheese# code an argument here
