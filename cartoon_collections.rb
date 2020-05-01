2412efe4fa3e8f1cbb2b7713ef4c303a6a261cd3
Matt Im <matthew.seiim@gmail.com>
2020-05-01 17:49:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,8 @@ def long_planeteer_calls(words)# code an argument here
   # Your code here
   short_words = []
   long_words = []
-  
+  if words.size > 4
+    
 end
 
 def find_the_cheese# code an argument here
